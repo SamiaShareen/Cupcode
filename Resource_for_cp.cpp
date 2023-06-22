d26451071c9c6bd0eb2c02da0654b756c90948d3
Samia Shareen <samiashareen369@gmail.com>
2023-06-22 20:47:50 +0600

    some resource

---
+++ Resource_for_cp.cpp
@@ -0,0 +1,24 @@
+/*
+Some_resource_for_learn_STL:
+
+1)geeksforgeeks is good for learn STL
+2)c++reference is also 
+3) nijader shika nijader chalai jaite hobe 
+4)Stack Overflow is our friend now ;)
+5)Topcoder Tutorial
+6) Junior Traning sheet-B topic:Study STL 
+
+
+Some_STL_Topic
+1)Vector (kinda array)
+2)string
+container(same_way_te_kaj_kora)
+*map
+*multimap
+*unorderedmap
+*set
+*multiset
+*unorderset
+3)sort
+4)reverse
+*/

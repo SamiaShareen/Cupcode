09facb526f62b4648a15d3ea8571b2dd7d8836bb
Samia Shareen <samiashareen369@gmail.com>
2023-06-29 11:09:11 +0600

    string train

---
+++ String.cpp/String-D.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+#define forcin(n) for(auto &x : n) cin >> x
+#define forcout(n) for(auto x : n) cout << x << " "
+#define sz(n) (int)n.size()
+#define el endl
+
+void printVec(vector<int> v , string s=""){
+    cout << s;
+    for(int i=0;i<(int)v.size();i++)
+        cout << v[i] << " ";
+    cout << "\n";
+}
+
+void solve(){
+    string s,t;
+    cin>>s;
+    cin.ignore();
+    getline(cin,t);
+    cout << sz(s) <<' '<< sz(t)<<el;
+    cout << s<<t<<el;
+    swap(s[0],t[0]);
+    cout << s <<' '<< t<<el;
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}

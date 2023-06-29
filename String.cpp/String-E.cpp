f246cd4947a35ea6f3983598d8990c4d287c1204
Samia Shareen <samiashareen369@gmail.com>
2023-06-29 11:18:00 +0600

    count

---
+++ String.cpp/String-E.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+#define forcin(n) for(auto &x : n) cin >> x
+#define forcout(n) for(auto x : n) cout << x << " "
+#define sz(n) (int)n.size()
+#define el endl
+
+void printVec(vector<int> v , string s=""){
+    cout << s;
+    for(int i=0;i<(int)v.size();i++)
+        cout << v[i] << " ";
+    cout << "\n";
+}
+
+void solve(){
+    string s;
+    int sum=0;
+    cin >> s;
+    for(auto x : s)
+        sum+=x-'0';
+    cout << sum << el;
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}

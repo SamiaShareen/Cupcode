17f4ea8a8a0f3f4f4fdbc75e523929e97327c3f9
Samia Shareen <samiashareen369@gmail.com>
2023-06-29 15:12:39 +0600

    ;/

---
+++ String.cpp/String-I.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+#define forcin(n) for(auto &x : n) cin >> x
+#define forcout(n) for(auto x : n) cout << x << " "
+#define sz(n) (int)n.size()
+#define el endl
+
+void printVec(vector<int> v , string s=""){
+    cout << s;
+    for(int i=0;i<(int)v.size();i++)
+        cout << v[i] << " ";
+    cout << "\n";
+}
+
+void solve(){
+    string s,t;
+    cin >> s;
+    t=s;
+    reverse(s.begin(),s.end());
+    //reverse(s.rbegin(),s.rend());         // same kaj korae :|
+    if(s==t) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}

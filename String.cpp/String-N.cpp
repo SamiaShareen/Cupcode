fef3dbc40485b8e036000c06fd45556579b814f6
Samia Shareen <samiashareen369@gmail.com>
2023-06-30 12:55:06 +0600

    Max subsequence

---
+++ String.cpp/String-N.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+#define forcin(n) for(auto &x : n) cin >> x
+#define forcout(n) for(auto x : n) cout << x << " "
+#define sz(n) (int)n.size()
+#define el endl
+
+void printVec(vector<int> v , string s=""){
+    cout << s;
+    for(int i=0;i<(int)v.size();i++)
+        cout << v[i] << " ";
+    cout << "\n";
+}
+
+void solve(){
+    int n,i,cnt=0;
+    string s;
+    cin >> n;
+    cin >> s;
+    for(i=0;i<n-1;i++){
+        if(s[i]!=s[i+1]){
+            cnt++;
+        }
+    }
+    cout << cnt+1 << el;
+}
+
+int main(){
+    int t=1;
+    //cin >> t;          // remove '//' for testcase
+    while(t--){
+        solve();
+    }
+    return 0;
+}

5f0e39a796ec9b03b47f459d8bce3a64eb5cb1e3
Samia Shareen <samiashareen369@gmail.com>
2023-07-03 12:15:09 +0600

    by 2d String

--- String.cpp/String-S.cpp
+++ String.cpp/String-S.cpp
@@ -32,6 +32,7 @@ void solve(){
         }
     }
     cout << cnt << el;
+
     for(i=0;s[i];i++){
         if(s[i]=='L') l++;
         else r++;
@@ -45,11 +46,33 @@ void solve(){
     }
 }
 
+// another sol^n by 2D string 
+
+void solve_2(){
+    int l=0,r=0,cnt=0,i,j=0;
+    char s[1005],t[1000][1000];
+    cin >> s;
+    for(i=0;s[i];i++,j++){
+        t[cnt][j]=s[i];
+        if(s[i]=='L') l++;
+        else r++;
+        if(l==r){
+            cnt++;
+            l=r=0;
+            j=-1;
+        }
+    }
+    cout << cnt << el;
+    for(i=0;i<cnt;i++)
+        cout << t[i] << el;
+}
+
 int main(){
     int t=1;
     //cin >> t;          // remove '//' for testcase
     while(t--){
-        solve();
+        //solve();
+        solve_2();
     }
     return 0;
 }
